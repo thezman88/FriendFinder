f6c8d7edaf5ea4fa779a392d3140ee46a89775eb
Zachary Cohen <zacharycohen@Zacharys-MBP.home>
2019-11-06 18:11:49 -0500

    fixed get routes

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -10,7 +10,7 @@ module.exports = function(app){
   });
 
   //a USE route to home page
-  app.use(function (req, res) {
+  app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname + '/../public/home.html'));
   });
 };
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Dependencies:
 var express = require('express');
 var bodyParser = require('body-parser');
-var path = require('path');
+// var path = require('path');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -9,8 +9,8 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+// app.use(bodyParser.text());
+// app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 //ROUTER
 require('./app/routing/api-routes.js')(app);
