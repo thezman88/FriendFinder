f386294295138a5fa6ba3b142786554451fcc744
Zachary Cohen <zacharycohen@Zacharys-MBP.home>
2019-11-06 19:27:48 -0500

    path

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 //Dependencies:
 var express = require('express');
 var bodyParser = require('body-parser');
-// var path = require('path');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
